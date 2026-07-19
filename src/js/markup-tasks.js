9d468ad4fcbfb78cfe340d9c9c2ff76a18e5e6dc
Vladyslav <vsemencenko15@gmail.com>
2026-07-19 19:27:11 +0300

    markup

---
+++ src/js/markup-tasks.js
@@ -0,0 +1,7 @@
+export function markup({name, description}) {
+  return `<li class="task-list-item">
+        <button class="task-list-item-btn">Delete</button>
+        <h3>${name}</h3>
+        <p>${description}</p>
+      </li>`
+}
